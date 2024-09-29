3f81dadc9f8d37856444cf4a2bf9a2ba2ba4fedc
Peter <pqiu85674@gmail.com>
2024-09-29 18:17:01 +0800

    update

--- ECPay/query_trade_info.mjs
+++ ECPay/query_trade_info.mjs
@@ -1,13 +1,13 @@
 import ecpay_payment from "ecpay-aio-node";
 import options from "./options.mjs";
 
-function query_trade_info(data) {
+async function query_trade_info(data) {
   console.log("成功進入query_trade_info function");
   const { MerchantTradeNo } = data;
 
   let base_param = { MerchantTradeNo };
   const query = new ecpay_payment(options);
-  const order = query.query_client.query_trade_info(base_param);
+  const order = await query.query_client.query_trade_info(base_param);
   console.log("成功撈取當筆交易資料:", order);
   return { status: "success", order };
 }
