e2e089ffde8c57dc9dc8f6513cde9d181ad5b735
Peter <pqiu85674@gmail.com>
2024-09-29 20:32:22 +0800

    update

--- ECPay/gen_chk_mac_value.mjs
+++ ECPay/gen_chk_mac_value.mjs
@@ -4,7 +4,7 @@ import options from "./options.mjs";
 function gen_chk_mac_value(data) {
   console.log("綠界金流回傳資料 data:", data);
 
-  const { CheckMacValue } = req.body;
+  const { CheckMacValue } = data;
   delete data.CheckMacValue; // 此段不驗證
 
   const create = new ecpay_payment(options);
