233ad2e3fffdd42eb1e5389fe46e4cc13275a628
Peter <pqiu85674@gmail.com>
2024-10-03 10:01:19 +0800

    update URL

--- server.mjs
+++ server.mjs
@@ -21,7 +21,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const whitelist = ["http://localhost:3000", "http://localhost:3001","https://pqiu85674.github.io/side-project"];
+const whitelist = ["http://localhost:3000", "http://localhost:3001","https://pqiu85674.github.io"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
